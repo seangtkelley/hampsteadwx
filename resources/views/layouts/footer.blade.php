4b919b08f5ce5400f82e137061b0aad754ca536d
Sean Kelley <sgtkode01@gmail.com>
2016-03-02 00:27:12 -0500

    footer fixing

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,22 +4,22 @@
 
     </div>-->
     <div class="col-md-12 col-lg-12" style="text-align: center; padding-left: 0px; padding-right: 0px">
-      <div class="well" style="margin-bottom: 0px;">
+      <div class="well" style="margin-bottom: 0px; border: none;">
         <div class="row betterRow">
           <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2" style="text-align: center">
-            <h4>Observations</h4>
+            <h4 style="padding-left:40px;" >Observations</h4>
             <ul>
-              <li><a href="#">Daily</a></li>
-              <li><a href="#">Weekly</a></li>
-              <li><a href="#">Max/Min Temps for Past 7 Days</a></li>
-              <li><a href="#">Max/Min Temps for Present Month</a></li>
+              <li><a href="http://www.nws.noaa.gov/view/validProds.php?prod=HYD&amp;node=KGYX" target="_blank">Daily</a></li>
+              <li><a href="http://www.nohrsc.noaa.gov/interactive/html/graph.html?station=HMPN3" target="_blank">Weekly</a></li>
+              <li><a href="http://mesonet.agron.iastate.edu/sites/plot.php?prod=0&station=HMPN3&network=NH_COOP" target="_blank">Max/Min Temps for Past 7 Days</a></li>
+              <li><a href="http://mesonet.agron.iastate.edu/sites/plot.php?prod=1&station=HMPN3&network=NH_COOP" target="_blank">Max/Min Temps for Present Month</a></li>
             </ul>
           </div>
           <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-            <h4>Summaries</h4>
+            <h4 style="padding-left:40px;">Summaries</h4>
             <ul>
-              <li><a href="#">Monthly</a></li>
-              <li><a href="#">Annual</a></li>
+              <li><a href="{{route('summaries.monthly.home')}}">Monthly</a></li>
+              <li><a href="{{route('summaries.annual.home')}}">Annual</a></li>
               <li><a href="#">Snow Season</a></li>
               <li><a href="#">Sunset Lake: Ice In/Ice Out</a></li>
               <li><a href="#">Peak Foliage</a></li>
@@ -27,38 +27,27 @@
             </ul>
           </div>
           <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-            <h4>Events</h4>
+            <h4 style="padding-left:40px;">Events</h4>
             <ul>
-              <li><a href="#">Daily</a></li>
-              <li><a href="#">Weekly</a></li>
-              <li><a href="#">Max/Min Temps for Past 7 Days</a></li>
-              <li><a href="#">Max/Min Temps for Present Month</a></li>
+              <li><a href="{{route('events.home')}}">All Events</a></li>
             </ul>
           </div>
           <div class="col-xs-6 col-sm-6 col-xs-2 col-sm-2 col-md-2 col-lg-2">
-            <h4>Normals</h4>
+            <h4 style="padding-left:40px;">Normals</h4>
             <ul>
-              <li><a href="#">All Normals</a></li>
-              <li><a href="#">Air Temperature</a></li>
-              <li><a href="#">Precipitation</a></li>
-              <li><a href="#">Heating/Cooling Degree Days</a></li>
-              <li><a href="#">Snowfall</a></li>
+              <li><a href="{{route('normals')}}">All Normals</a></li>
             </ul>
           </div>
           <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-            <h4>Info</h4>
+            <h4 style="padding-left:40px;">Info</h4>
             <ul>
-              <li><a href="#">All Info</a></li>
+              <li><a href="{{route('info')}}">All Info</a></li>
             </ul>
           </div>
           <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
-            <h4>Photos</h4>
+            <h4 style="padding-left:40px;">Photos</h4>
             <ul>
-              <li><a href="#">Winter</a></li>
-              <li><a href="#">Spring</a></li>
-              <li><a href="#">Summer</a></li>
-              <li><a href="#">Autumn</a></li>
-              <li><a href="#">Nearby Stations</a></li>
+              <li><a href="{{route('photos.home')}}">All Photos</a></li>
             </ul>
           </div>
         </div>
