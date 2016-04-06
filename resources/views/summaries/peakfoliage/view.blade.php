e188399da9ae828c745b855fd97e663f679c6567
Sean Kelley <sgtkode01@gmail.com>
2016-04-06 00:03:25 -0400

    reformatted peak foliage summary

--- resources/views/summaries/peakfoliage/view.blade.php
+++ resources/views/summaries/peakfoliage/view.blade.php
@@ -8,16 +8,16 @@
 @section('content')
   <div id="" class="container" style="min-height: 650px;">
     <h3 style="text-align: center;">Peak Foliage</h3>
-    <table class="table-bordered table-hover" style="min-width: 150px">
+    <table class="table-bordered table-hover" style="width: 100%;">
       <tbody>
         <tr>
-          <th style="text-align: center;"><h4>Year</h4></th>
-          <th style="text-align: center;"><h4>Date</h4></th>
+          <th style="text-align: center;"><h4><b>Year<b></h4></th>
+          <th style="text-align: center;"><h4><b>Date<b></h4></th>
         </tr>
         @foreach ($allPeaks as $peak)
           <tr>
-            <th style="text-align: center;">{{ $peak->year }}</th>
-            <th style="text-align: center;">{{ $peak->date }}</th>
+            <th style="text-align: center;"><h4>{{ $peak->year }}</h4></th>
+            <th style="text-align: center;"><h4>{{ $peak->date }}</h4></th>
           </tr>
         @endforeach
       </tbody>
