031cb00b6a4cd4eb23c5330bd37e35ac8197339a
Sean Kelley <sgtkode01@gmail.com>
2016-07-20 12:17:27 -0400

    added js-cookie library; added global function for determining mobile; added alert for first time visitors on mobile to use landscape mode

--- gulpfile.js
+++ gulpfile.js
@@ -15,57 +15,59 @@ var elixir = require('laravel-elixir');
 elixir.config.sourcemaps = false;
 
 var paths = {
-    'jquery'              : './bower_components/jquery/',
-    'bootstrap'           : './bower_components/bootstrap-sass-official/assets/',
-    'select2'             : './bower_components/select2/dist/',
-    'jqueryFileUpload'    : './bower_components/jquery-file-upload/',
-    'jqueryUI'            : './bower_components/jquery-ui/',
-    'moment'              : './bower_components/moment/'
+    'jquery': './bower_components/jquery/',
+    'bootstrap': './bower_components/bootstrap-sass-official/assets/',
+    'select2': './bower_components/select2/dist/',
+    'jqueryFileUpload': './bower_components/jquery-file-upload/',
+    'jqueryUI': './bower_components/jquery-ui/',
+    'moment': './bower_components/moment/',
+    'jscookie': './bower_components/js-cookie/'
 };
 
-elixir(function(mix) {
-	//mix.less('app.less');
+elixir(function (mix) {
+    //mix.less('app.less');
 
-  // compile scss files from resources/assets/sass
-  mix.sass("main.scss", 'public/css/main.css', {includePaths: [paths.bootstrap + 'stylesheets/']});
-  mix.sass("raw.scss", 'public/css/raw.css', {includePaths: [paths.bootstrap + 'stylesheets/']});
+    // compile scss files from resources/assets/sass
+    mix.sass("main.scss", 'public/css/main.css', {includePaths: [paths.bootstrap + 'stylesheets/']});
+    mix.sass("raw.scss", 'public/css/raw.css', {includePaths: [paths.bootstrap + 'stylesheets/']});
 
-  // add more if need be
-  //mix.sass("<filename>.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']});
+    // add more if need be
+    //mix.sass("<filename>.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']});
 
-  // create the addons css
-  mix.copy([
-    paths.jqueryFileUpload + 'css/jquery.fileupload.css',
-    paths.jqueryUI + 'themes/smoothness/jquery-ui.css',
-    paths.select2 + 'css/select2.css'
-  ], 'resources/assets/css');
-  mix.styles([
-    'jquery.fileupload.css',
-    'jquery-ui.css',
-    'select2.css'
-  ], 'public/css/addons.css');
+    // create the addons css
+    mix.copy([
+        paths.jqueryFileUpload + 'css/jquery.fileupload.css',
+        paths.jqueryUI + 'themes/smoothness/jquery-ui.css',
+        paths.select2 + 'css/select2.css'
+    ], 'resources/assets/css');
+    mix.styles([
+        'jquery.fileupload.css',
+        'jquery-ui.css',
+        'select2.css'
+    ], 'public/css/addons.css');
 
-  // copy over jquery ui images
-  mix.copy(paths.jqueryUI + 'themes/smoothness/images/**', 'public/css/images');
+    // copy over jquery ui images
+    mix.copy(paths.jqueryUI + 'themes/smoothness/images/**', 'public/css/images');
 
-  // copy over bootstrap fonts
-  mix.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts');
+    // copy over bootstrap fonts
+    mix.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts');
 
-  // combine bootstrap and jquery scripts into public/js/app.js
-  mix.scripts([
-    paths.jquery + "dist/jquery.js",
-    paths.jqueryUI + "jquery-ui.js",
-    paths.bootstrap + "javascripts/bootstrap.js",
-    paths.jqueryFileUpload + 'js/jquery.iframe-transport.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload-ui.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload-jquery-ui.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload-process.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload-image.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload-audio.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload-video.js',
-    paths.jqueryFileUpload + 'js/jquery.fileupload-validate.js',
-    paths.select2 + 'js/select2.full.js',
-    paths.moment + 'moment.js',
-  ], 'public/js/app.js', './');
+    // combine bootstrap and jquery scripts into public/js/app.js
+    mix.scripts([
+        paths.jquery + "dist/jquery.js",
+        paths.jqueryUI + "jquery-ui.js",
+        paths.bootstrap + "javascripts/bootstrap.js",
+        paths.jqueryFileUpload + 'js/jquery.iframe-transport.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload-ui.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload-jquery-ui.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload-process.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload-image.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload-audio.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload-video.js',
+        paths.jqueryFileUpload + 'js/jquery.fileupload-validate.js',
+        paths.select2 + 'js/select2.full.js',
+        paths.moment + 'moment.js',
+        paths.jscookie + 'src/js.cookie.js'
+    ], 'public/js/app.js', './');
 });
